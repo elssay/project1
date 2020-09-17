95f38b1535c4c9e8e98807c023ad665b10a0b145
elssay <sveta-zvezda-poeta@yandex.ru>
2020-09-17 15:07:32 +0300

    initial

---
+++ prog1.py
@@ -0,0 +1,19 @@
+slide1 = '(^_^)'
+slide2 = '(^_~)'
+slide3 = '(^_^)'
+slide4 = '(о_О)'
+slide5 = '(O_O)'
+slide6 = 'Hello, World!'
+import time
+
+print(slide1)
+time.sleep(1)
+print(slide2)
+time.sleep(1)
+print(slide3)
+time.sleep(1)
+print(slide4)
+time.sleep(1)
+print(slide5)
+time.sleep(3)
+print(slide6)
